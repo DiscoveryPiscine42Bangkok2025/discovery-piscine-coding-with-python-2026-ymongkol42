252eb8797e2f7cbb97960fa83430ecd7acdf76b1
ymongkol42 <66010675@kmitl.ac.th>
2026-02-10 09:38:16 +0700

    submit cell01 ex01

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,3 +1,3 @@
 first_name = "Yuthika";
 last_name = "Mongkolgat";
-print(f" ",first_name,last_name);
+print(f"{first_name} {last_name}");
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Yuthika";
+last_name = "Mongkolgat";
+whole_name = f"{first_name} {last_name}"
+print(whole_name);
